006877987c93b92e25235baefd5b07ee8e5113a2
Yash <yash360@gmail.com>
2013-11-04 22:25:13 +0530

    fixed approx search script ;)

--- scripts/new_test/search.py
+++ scripts/new_test/search.py
@@ -1,17 +1,21 @@
 import subprocess
 import glob
+from math import floor
 
 
-def findApproxMatch(searchQuery):
-    command = 'agrep'
-    flags = '-i'
+def findApproxMatch(searchQuery, errorPercent=0.0):
+    err = int(floor(errorPercent * len(searchQuery)))
+    grep = 'agrep'
+    flags = '-i'+str(err)
     searchQuery = searchQuery
-    searchDir = "/home/yash/work/subtitle-data/"
+    searchDir = "/home/yash/work/subtitle-data/*"
+
+    command = grep + " " + flags + " " + "'" + searchQuery + "'" + " " + searchDir
+    print 'Executing command: ' +command
     
-    args = [command, flags,  searchQuery ] + glob.glob(searchDir+'*')
-    output = subprocess.Popen( args, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate()
+    output = subprocess.Popen( command, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE).communicate()
     print output
 
 
 if __name__=='__main__':
-    findApproxMatch('minimum safe distance')
+    findApproxMatch('distao', 0.3)
